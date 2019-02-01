177ccaa5389091cdbdd3d391c93443e2cfdbd246
devmessias <messias.physics@gmail.com>
2019-02-01 20:17:56 -0200

    This closes #2

--- emate/utils/misc.py
+++ emate/utils/misc.py
@@ -1,53 +1,36 @@
-
 import tensorflow as tf
-import numpy as np
 
 
-def random_vec_factory(
-    H,
-    dimension,
-    num_vecs,
-    tf_float=tf.float32,
-    tf_complex=tf.complex64,
-    name_scope=None
-):
-    """
-    Generates a set of complex random vectors
-    Args:
-        H: SparseTensor(shape=(dimension, dimension), dtype=tf_complex)
-        dimension: (int) dimension matrix
-        num_vecs: (int) number of random vectors
-        tf_float: (tensorflow float type)
-        tf_complex: (tensorflow complex type)
-        name_scope: (str)(default="random_vec_factory")
-            scope name for tensorflow
+def replace_by_indices(input_matrix, values, indices, dimension,
+                       tf_float=tf.float32):
+    with tf.name_scope("replace_by_indices"):
 
-    Return:
-        alpha0: Tensor(shape=(dimension, num_vecs), dtype=tf_complex)
-        alpha1: Tensor(shape=(dimension, num_vecs), dtype=tf_complex)
+        identity_matrix = tf.eye(dimension, dtype=tf_float)
 
-    """
-    with tf.name_scope(name_scope, "random_vec_factory") as scope:
-
-        random_phases = 2.*np.pi*tf.random.uniform(
-            [dimension, num_vecs],
-            dtype=tf_float
+        mask_values = tf.tile([0.0], [tf.shape(indices)[0]])
+        mask_sparse_tensor = tf.SparseTensor(
+            indices,
+            mask_values,
+            tf.shape(input_matrix, out_type=tf.int64)
         )
-        alpha0_sin = tf.sin(
-            random_phases
-        )
-        alpha0_cos = tf.cos(
-            random_phases
-        )
-        alpha0 = tf.add(
-            tf.cast(alpha0_cos, dtype=tf_complex),
-            1j*tf.cast(alpha0_sin, dtype=tf_complex)
+        # tf.sparse_tensor_dense_matmul(.., I) can be replaced by
+        # tf.sparse.todense. However, the last has no GPU suport until now
+        masked_input_tensor = tf.multiply(
+            input_matrix,
+            tf.sparse_tensor_dense_matmul(mask_sparse_tensor, identity_matrix)
         )
 
-        alpha1_sin = tf.sparse_tensor_dense_matmul(H, alpha0_sin)
-        alpha1_cos = tf.sparse_tensor_dense_matmul(H, alpha0_cos)
-        alpha1 = tf.add(
-            tf.cast(alpha1_cos, dtype=tf_complex),
-            1j*tf.cast(alpha1_sin, dtype=tf_complex)
+        values_sparse_tensor = tf.SparseTensor(
+            indices,
+            values,
+            tf.shape(input_matrix, out_type=tf.int64)
         )
-        return alpha0, alpha1
+        output_matrix = tf.add(
+            masked_input_tensor,
+            tf.sparse_tensor_dense_matmul(
+                values_sparse_tensor,
+                identity_matrix
+            )
+        )
+
+        return output_matrix
