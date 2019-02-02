2208f6f46c8a925833764c0dd555d100a26c3bf5
devmessias <messias.physics@gmail.com>
2019-02-02 14:34:09 -0200

    replace_by_indices now works with matrices with arbitrary shape.
    Before this commit was necessary to pass the dimension argument, that it is not necessary anymore.

--- emate/utils/tf.py
+++ emate/utils/tf.py
@@ -15,7 +15,7 @@ Available methods
 import tensorflow as tf
 
 
-def replace_by_indices(input_matrix, values, indices, dimension,
+def replace_by_indices(input_matrix, values, indices,
                        tf_type=tf.float32, name_scope=None):
     """
     Given an input_matrix, replaces the values given a set of indices.
@@ -25,7 +25,6 @@ def replace_by_indices(input_matrix, values, indices, dimension,
         input_matrix: Tensor(shape=(dimension, dimension), dtype=tf_type)
         values: Tensor(shape=(None,), dtype=tf_type)
         indices: Tensor(shape=(None, 2), dtype=int64)
-        dimension: (int) or (tensorflow int constant)
         tf_type: (tensorflow type)(default=tf.float32)
         name_scope: (str)(default="replace_by_indices")
             scope name for tensorflow
@@ -35,24 +34,30 @@ def replace_by_indices(input_matrix, values, indices, dimension,
         output_matrix: Tensor(shape=(dimension, dimension), dtype=tf_type)
 
     """
+    
     with tf.name_scope("replace_by_indices", name_scope):
 
-        identity_matrix = tf.eye(dimension, dtype=tf_type)
+        identity_matrix = tf.eye(
+            tf.shape(input_matrix, out_type=tf.int64)[1],
+            dtype=tf_type
+        )
+        ones = tf.ones(tf.shape(input_matrix), dtype=tf_type)
+        
 
-        mask_values = tf.tile(
-            tf.constant([0.0], dtype=tf_type),
-            [tf.shape(indices)[0]]
-        )
-        mask_sparse_tensor = tf.SparseTensor(
+        mask_sparse_tensor_ones = tf.SparseTensor(
             indices,
-            mask_values,
-            tf.shape(input_matrix, out_type=tf.int64)
+            values=tf.tile(
+                tf.constant([-1.0], dtype=tf_type),
+                [tf.shape(indices)[0]]
+            ),
+            dense_shape=tf.shape(input_matrix, out_type=tf.int64)
         )
-        # tf.sparse_tensor_dense_matmul(.., I) can be replaced by
-        # tf.sparse.todense. However, the last has no GPU suport until now
+        
+        mask_values = tf.add(ones, tf.sparse_tensor_dense_matmul(mask_sparse_tensor_ones,identity_matrix))
+       
         masked_input_tensor = tf.multiply(
             input_matrix,
-            tf.sparse_tensor_dense_matmul(mask_sparse_tensor, identity_matrix)
+            mask_values
         )
 
         values_sparse_tensor = tf.SparseTensor(
@@ -62,10 +67,8 @@ def replace_by_indices(input_matrix, values, indices, dimension,
         )
         output_matrix = tf.add(
             masked_input_tensor,
-            tf.sparse_tensor_dense_matmul(
-                values_sparse_tensor,
-                identity_matrix
-            )
+
+            tf.sparse_tensor_dense_matmul(values_sparse_tensor,identity_matrix)
         )
 
         return output_matrix
