843f79e000b1a2b5de8406aee48684ba5237f3f1
devmessias <messias.physics@gmail.com>
2020-07-05 19:37:02 -0300

    Fix the tf version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(exclude=["build", ]),
     long_description=README_TEXT,
     long_description_content_type="text/markdown",
-    install_requires=["tensorflow==1.13.2", "scipy", "numpy"],
+    install_requires=["tensorflow==1.15.0", "scipy", "numpy"],
     include_package_data=True,
     license="MIT",
     description="",
