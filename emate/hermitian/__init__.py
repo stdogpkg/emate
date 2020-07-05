cddd1069340c6689c22edd4240f48ae8e012bdbc
devmessias <messias.physics@gmail.com>
2020-07-05 19:48:54 -0300

    Fix missing import cupykpm

--- emate/hermitian/__init__.py
+++ emate/hermitian/__init__.py
@@ -1,4 +1,4 @@
 from emate.hermitian.kpm import pykpm, cupykpm
 from emate.hermitian import tfops, cupyops
 
-__all__ = ["pykpm", "tfops", "cukpm", "cupykpm"]
+__all__ = ["pykpm", "tfops", "cupykpm", "cupykpm"]
